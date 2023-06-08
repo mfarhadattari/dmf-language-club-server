e89f6706e4816a42d16eb9b144866da1ec541fa4
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-08 21:07:17 +0600

    get popular instructor

--- routes/public.js
+++ routes/public.js
@@ -11,4 +11,14 @@ router.get("/popular-classes", async (req, res) => {
   res.send(result);
 });
 
+router.get("/popular-instructor", async (req, res) => {
+  const userCollection = req.userCollection;
+  const query = { role: "instructor" };
+  const option = {
+    sort: { totalStudent: -1 },
+  };
+  const result = await userCollection.find(query, option).limit(6).toArray();
+  res.send(result);
+});
+
 module.exports = router;
