af86d00e10c23558a1ccd6a1bae1127fd717554e
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-10 14:13:21 +0600

    get user role, user token verify

--- routes/other.js
+++ routes/other.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const { jwtVerify } = require("../middleware/middleware");
 require("dotenv").config();
 const route = express.Router();
 
@@ -7,7 +8,18 @@ const route = express.Router();
 route.post("/generate-jwt", (req, res) => {
   const data = req.body;
   const token = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1h" });
-  res.send({token: token});
+  res.send({ token: token });
 });
 
+// !--------------- USER ROLE ---------------! //
+route.get("/user-role", jwtVerify, async (req, res) => {
+  const userCollection = req.userCollection;
+  const email = req.query.email;
+  if (email !== req.decoded.email) {
+    return res.status(403).send({ error: true, message: "Access Forbidden" });
+  }
+  const query = { email: email };
+  const result = await userCollection.findOne(query);
+  res.send({ role: result?.role || student });
+});
 module.exports = route;
