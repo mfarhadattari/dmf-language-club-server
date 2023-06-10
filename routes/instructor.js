2fc7bff9d31389df1a9b6c7c887c0a6a61acbcf4
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-11 00:51:37 +0600

    my class api for instructor

--- routes/instructor.js
+++ routes/instructor.js
@@ -2,11 +2,21 @@ const express = require("express");
 const { jwtVerify, instructorVerify } = require("../middleware/middleware");
 const router = express.Router();
 
+// !------------------- Add Class -------------------! //
 router.post("/add-class", jwtVerify, instructorVerify, async (req, res) => {
   const classCollection = req.classCollection;
   const data = req.body;
   console.log(data);
   const result = await classCollection.insertOne(data);
+  res.send(result);
+});
+
+// !------------------ My Class -------------------! //
+router.get("/my-classes", jwtVerify, instructorVerify, async (req, res) => {
+  const classCollection = req.classCollection;
+  const email = req.query.email;
+  const query = { instructorEmail: email };
+  const result = await classCollection.find(query).toArray();
   res.send(result);
 });
 
