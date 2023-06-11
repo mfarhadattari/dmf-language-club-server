8b6174629ab0b15853be48789de8cfe7158f8b4d
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-11 08:44:03 +0600

    Approve API Implemantation

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const { jwtVerify, adminVerify } = require("../middleware/middleware");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 
 router.get("/users", jwtVerify, adminVerify, async (req, res) => {
   const userCollection = req.userCollection;
   const result = await userCollection.find().toArray();
   res.send(result.reverse());
-
 });
 router.get("/classes", jwtVerify, adminVerify, async (req, res) => {
   const classCollection = req.classCollection;
   const result = await classCollection.find().toArray();
   res.send(result.reverse());
+});
+
+router.patch("/approve-class/:id", jwtVerify, adminVerify, async (req, res) => {
+  const classCollection = req.classCollection;
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const updateStatus = {
+    $set: {
+      status: "approve",
+    },
+  };
+  const result = await classCollection.updateOne(query, updateStatus);
+  res.send(result);
 });
 
 module.exports = router;
