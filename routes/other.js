448dbbf06c4afe532a5e56698dd3daebbcc8406a
Mohammad Farhad <mfarhad.attari@gmail.com>
2023-06-16 16:45:32 +0600

    Update user profile API

--- routes/other.js
+++ routes/other.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const { jwtVerify } = require("../middleware/middleware");
+const { ObjectId } = require("mongodb");
 require("dotenv").config();
 const router = express.Router();
 
@@ -45,6 +46,29 @@ router.get("/user-profile", jwtVerify, async (req, res) => {
   }
   const query = { email: email };
   const result = await userCollection.findOne(query);
+  res.send(result);
+});
+
+// !--------------- Update User Profile ------------! //
+router.post("/update-user-profile", jwtVerify, async (req, res) => {
+  const userCollection = req.userCollection;
+  const email = req.query.email;
+  if (email !== req.decoded.email) {
+    return res.status(403).send({ error: true, message: "Access Forbidden" });
+  }
+  const data = req.body;
+  const id = data._id;
+  const query = { _id: new ObjectId(id), email: email };
+  const updateInfo = {
+    $set: {
+      displayName: data?.displayName,
+      photoURL: data?.photoURL,
+      phone: data?.phone,
+      gender: data?.gender,
+      address: data?.address,
+    },
+  };
+  const result = await userCollection.updateOne(query, updateInfo);
   res.send(result);
 });
 
